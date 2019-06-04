f5af4ee41b000dacde57e76f9e8f452f4064659e
Leonardo Dias <leo.thdias@gmail.com>
2019-06-04 10:06:52 -0300

    Implemented retrieve files from folder to generate base64string

--- Images/Program.cs
+++ Images/Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
 using System.IO;
 using System.Text;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Images
 {
     class Program
     {
         static readonly string rootFolder = @"C:\Users\Leonardot\Documents\Projetos\Ref\CSharp_Playground\Images";
-        static readonly string imageFolderPath = @"\images";
-        static readonly string base64FolderPath = @"\base64";
+        static readonly string imageFolderPath = @"\images\";
+        static readonly string base64FolderPath = @"\base64\";
         static void Main(string[] args)
         {
-            string imagePath = @"\COMPROVANTE_DE_RENDIMENTO_MARIDO.jpg";
-            string base64 = Base64String(imagePath);
+            List<string> files = Directory.GetFiles(rootFolder + imageFolderPath).ToList();
+            
+            foreach(string file in files) {
+                string imagePath = file;
+                string base64 = Base64String(imagePath);
 
-            using(StreamWriter writer = new StreamWriter(rootFolder + base64FolderPath + @"\base64.txt"))
-            {
-                writer.WriteLine(base64);
+                string base64file = Path.GetFileNameWithoutExtension(file);
+                
+                using(StreamWriter writer = new StreamWriter(rootFolder + base64FolderPath + base64file + ".txt"))
+                {
+                    writer.Write(base64);
+                }
             }
         }
 
         static string Base64String(string imagePath) 
         {
-            if (File.Exists(rootFolder + imageFolderPath + imagePath)){
-                byte[] imageByte = File.ReadAllBytes(rootFolder + imageFolderPath + imagePath);
+            if (File.Exists(imagePath)){
+                byte[] imageByte = File.ReadAllBytes(imagePath);
                 return Convert.ToBase64String(imageByte);
             } else {
                 return null;
